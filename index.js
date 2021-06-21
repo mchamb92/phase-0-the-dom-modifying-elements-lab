6dbdc1a4973c029f604f8c79ea11d8ae368658cf
Mustafa <mchambqa@gmail.com>
2021-06-21 02:12:48 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.getElementById('main');
+
+main.remove();
+
+const h1 = document.createElement('h1');
+
+h1.id = "victory";
+
+const newHeader = h1;
+
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
